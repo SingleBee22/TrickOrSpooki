f0bded0b42045b66ccba5f251d3355777c0bc598
SingleBee22 <kittycc2207@gmail.com>
2023-07-10 02:40:16 -0400

    1.4
    Olvide unos assets, aqui van:]
    corte los sprites igual, estan listos para usar

--- Assets/Scripts Generales/Menus/Start_Button.cs
+++ Assets/Scripts Generales/Menus/Start_Button.cs
@@ -3,10 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class Start_Button : MonoBehaviour
 {
-    public string Movie_Intro; 
+    //public string Movie_Intro; 
+    public string Tutorial; //Mientras la pelicula no este hecha, no cambien esto!!
 
     public void StartGame()
     {
-        SceneManager.LoadScene(Movie_Intro); 
+        SceneManager.LoadScene(Tutorial); //Recuerden cambiar esto si pasaran a la pelicula :]
     }
 }
